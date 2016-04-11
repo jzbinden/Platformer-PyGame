66a3a3e539427d03a11da565cf06a8a8c18f41cc
jzbinden <jan.zbinden@hotmail.ch>
2016-04-11 18:51:34 +0200

    Creating the main file

--- main.py
+++ main.py
@@ -1,3 +1,17 @@
+# Global constants
+
+# Colors
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+GREEN = (0, 255, 0)
+RED = (255, 0, 0)
+BLUE = (0, 0, 255)
+
+# Screen dimensions
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
+
+
 def main():
     """ Main Program """
     pygame.init()
